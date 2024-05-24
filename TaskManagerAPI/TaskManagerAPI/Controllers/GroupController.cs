51a451af36364baa8bc1b5339ea70b2794decbf1
NIAGARA\darkl <ivan.andrau@gmail.com>
2024-05-24 20:58:01 +0200

    initial variant

---
+++ TaskManagerAPI/TaskManagerAPI/Controllers/GroupController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TaskManagerAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GroupController : ControllerBase
+    {
+        [HttpGet(Name = "GetGroups")]
+        public IEnumerable<string> Get()
+        {
+            var groups = new[] { "IvanGr", "LarryGr", "PavelGr" };
+            return groups;
+        }
+
+        [HttpPut(Name = "AddGroups")]
+        public IEnumerable<string> AddGroups()
+        {
+            var groups = new[] { "IvanGr", "LarryGr", "PavelGr" };
+            return groups;
+        }
+    }
+}
